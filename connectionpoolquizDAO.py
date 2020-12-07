486cf9673f7961eca179c9a0cec74e70251d09e8
ocoleman <owencoleman@outlook.ie>
2020-12-07 00:38:36 +0000

    pushing server files

---
+++ connectionpoolquizDAO.py
@@ -0,0 +1,138 @@
+import mysql.connector
+import dbconfig as cfg
+class QuizDAO:
+    
+    def initConnectToDB(self):
+        db = mysql.connector.connect(
+            host=       cfg.mysql['host'],
+            user=       cfg.mysql['user'],
+            password=   cfg.mysql['password'],
+            database=   cfg.mysql['database'],
+            pool_name='my_connection_pool',
+            pool_size=10
+        )
+        return db
+
+    def getConnection(self):
+        db = mysql.connector.connect(
+            pool_name='my_connection_pool'
+        )
+        return db
+
+    def __init__(self): 
+        db=self.initConnectToDB()
+        db.close()
+
+    def createUser(self, username, password):
+        db = self.getConnection()
+        cursor = db.cursor()
+        sql="INSERT INTO user (username, password) VALUES (%s,%s)"
+        values=(username,password)
+        cursor.execute(sql, values)
+        db.commit()
+        db.close()
+        
+    def getUser(self, username, password):
+        db = self.getConnection()
+        cursor = db.cursor(dictionary=True)
+        sql="select * from user where username=%s and password=%s"
+        values=(username,password)
+        cursor.execute(sql, values)
+        result = cursor.fetchone()
+        db.close()
+        return result
+    
+    def checkForUser(self, username):
+        db = self.getConnection()
+        cursor = db.cursor(dictionary=True)
+        sql="select * from user where username=%s"
+        values=(username,)
+        cursor.execute(sql, values)
+        result = cursor.fetchone()
+        return result
+
+
+    def create(self, question):
+        db = self.getConnection()
+        cursor = db.cursor()
+        sql="insert into questions (author,category,correct_answer,difficulty,incorrect_answers,question,type) values (%s,%s,%s,%s,%s,%s,%s)"
+        values = [
+            question["author"],
+            question["category"],
+            question["correct_answer"],
+            question["difficulty"],
+            question["incorrect_answers"],
+            question["question"],
+            question["type"],
+        ]
+        cursor.execute(sql, values)
+        db.commit()
+        lastRowId=cursor.lastrowid
+        db.close()
+        return lastRowId
+
+    def getAll(self):
+        db = self.getConnection()
+        cursor = db.cursor()
+        sql="select * from questions"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        returnArray = []
+        print(results)
+        for result in results:
+            print(result)
+            returnArray.append(self.convertToDictionary(result))
+        db.close()
+        return returnArray
+
+    def findById(self, id):
+        db = self.getConnection()
+        cursor = db.cursor()
+        sql="select * from questions where id = %s"
+        values = (id,)
+
+        cursor.execute(sql, values)
+        result = cursor.fetchone()
+        question=self.convertToDictionary(result)
+        db.close()
+        return question
+
+    def update(self, values):
+        db = self.getConnection()
+        cursor = db.cursor()
+        sql="update questions set author = %s,category = %s,correct_answer = %s,difficulty = %s,incorrect_answers = %s,question = %s,type = %s  where id = %s"
+        cursor.execute(sql, values)
+        db.commit()
+        db.close()
+
+    def delete(self, id):
+        db = self.getConnection()
+        cursor = db.cursor()
+        sql="delete from questions where id = %s"
+        values = (id,)
+        cursor.execute(sql, values)
+        db.commit()
+        db.close()
+        
+    def getQuestionIds(self):
+        db = self.getConnection()
+        cursor = db.cursor()
+        sql = "SELECT id FROM questions"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        db.close()
+        return results
+
+
+    def convertToDictionary(self, result):
+        colnames=['id','author','category',"correct_answer","incorrect_answers","type","difficulty","question"]
+        item = {}
+        
+        if result:
+            for i, colName in enumerate(colnames):
+                value = result[i]
+                item[colName] = value
+        
+        return item
+
+quizDAO = QuizDAO()
---
+++ server.py
@@ -0,0 +1,194 @@
+from flask import Flask, url_for, request, abort, redirect, jsonify, session, render_template, url_for
+from connectionpoolquizDAO import quizDAO as quizDAO
+import re
+app = Flask(__name__, template_folder='static')
+app.secret_key= 'somesecretkeyadaf23'
+
+
+@app.route('/')
+def home():
+    if 'loggedin' in session:
+
+        return render_template('home.html', username=session['username'])
+    
+    return redirect(url_for('login'))
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    msg = ""
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        account = quizDAO.getUser(username, password)
+
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+
+            return redirect(url_for('home'))
+        else:
+            msg = 'Incorrect username/password'
+
+    return render_template('index.html', msg=msg)
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+
+    return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+
+    msg = ""
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+
+        account = quizDAO.checkForUser(username)
+
+        #error handling
+        if account:
+            msg= "Error: Account already exists!"
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = "Error: Username must contain only letters and numbers!"
+        elif not username or not password:
+            msg = "Error: fields were filled incorrectly"
+        else:
+            #valid submission
+            quizDAO.createUser(username, password)
+            msg = "Registration Successful"
+            return redirect(url_for('login'))
+        
+    elif request.method == 'POST':
+        #form empty
+        msg = "Please fill out the form"
+
+    return render_template('register.html', msg=msg)
+
+@app.route('/play')
+def play():
+    qCount = quizDAO.getQuestionIds()
+    msg = [i[0] for i in qCount]
+
+    if 'loggedin' in session:
+
+            return render_template('playQuiz.html', username=session['username'], msg=msg,)
+
+@app.route('/OpenTrivaQuiz')
+def thirdpartyquiz():
+
+    if 'loggedin' in session:
+
+        return render_template('thirdpartyquiz.html', username=session['username'])
+    
+    return redirect(url_for('login'))
+
+@app.route('/createquestion')
+def createQuestion():
+
+    if 'loggedin' in session:
+
+        return render_template('createquestion.html', username=session['username'])
+    
+    return redirect(url_for('login'))
+
+
+@app.route('/questions')
+def getAll():
+    return jsonify(quizDAO.getAll())
+
+@app.route('/questions/<int:id>')
+def findById(id):
+    return jsonify(quizDAO.findById(id))
+
+
+@app.route('/questions', methods=['POST'])
+def create():
+
+    incorrect_answers = ""
+
+    if not request.json:
+        abort(400)
+    
+    for item in request.json["incorrect_answers"]:
+        if item != "":
+            incorrect_answers += item+"," 
+
+    question = {
+        "author": request.json["author"],
+        "category": request.json["category"],
+        "correct_answer": request.json["correct_answer"],
+        "difficulty": request.json["difficulty"],
+        "incorrect_answers": incorrect_answers,
+        "question": request.json["question"],
+        "type": request.json["type"]
+    }
+    return jsonify(quizDAO.create(question))
+
+@app.route('/browse')
+def browseQuestions():
+
+    if 'loggedin' in session:
+
+        return render_template('browse.html', username=session['username'])
+    
+    return redirect(url_for('login'))
+
+
+
+@app.route('/questions/<int:id>', methods=['PUT'])
+def update(id):
+    incorrect_answers = ""
+    foundQuestion = quizDAO.findById(id)
+    if not foundQuestion:
+        abort(404)
+    
+    if not request.json:
+        abort(400)
+    reqJson = request.json
+
+
+    if 'author' in reqJson:
+       foundQuestion['author'] = reqJson['author']
+    if 'category' in reqJson:
+       foundQuestion['category'] = reqJson['category']
+    if 'correct_answer' in reqJson:
+       foundQuestion['correct_answer'] = reqJson['correct_answer']
+    if 'difficulty' in reqJson:
+       foundQuestion['difficulty'] = reqJson['difficulty']
+
+    if 'incorrect_answers' in reqJson:
+        for item in request.json["incorrect_answers"]:
+            if item != "":
+                incorrect_answers += item+"," 
+        foundQuestion['incorrect_answers'] = incorrect_answers
+
+    if 'question' in reqJson:
+       foundQuestion['question'] = reqJson['question']
+    if 'type' in reqJson:
+       foundQuestion['type'] = reqJson['type']
+    
+    values = (foundQuestion['author'],
+            foundQuestion['category'],
+            foundQuestion['correct_answer'],
+            foundQuestion['difficulty'],
+            foundQuestion['incorrect_answers'],
+            foundQuestion['question'],
+            foundQuestion['type'],
+            foundQuestion['id'])
+
+    quizDAO.update(values)
+    return jsonify(foundQuestion)
+
+@app.route('/questions/<int:id>', methods=['DELETE'])
+def delete(id):
+    quizDAO.delete(id)
+    return jsonify({"done":True})
+
+if __name__ == "__main__":
+    print("in if")
+    app.run(debug=True) #allows for realtime editing
